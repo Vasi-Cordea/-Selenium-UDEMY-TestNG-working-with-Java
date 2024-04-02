4329919efc4aa9adc94d8ea7ba60f544aeee28c9
Vasi-Cordea <150058199+Vasi-Cordea@users.noreply.github.com>
2024-04-02 17:12:03 +0300

    class2

---
+++ src/LearningTestNG/TestNGAnotation_2.java
@@ -0,0 +1,11 @@
+package LearningTestNG;
+
+import org.testng.annotations.Test;
+
+public class TestNGAnotation_2 {
+    @Test
+    public void test3(){
+        System.out.println("Inside test 3");
+    }
+
+}
