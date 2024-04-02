d6af3e4f8ab0379896a9d20d970a8260a7da579e
Vasi-Cordea <150058199+Vasi-Cordea@users.noreply.github.com>
2024-04-02 15:20:37 +0300

    begin!

---
+++ src/LearningTestNG/TenstNGAnotation_1.java
@@ -0,0 +1,11 @@
+package LearningTestNG;
+
+import org.testng.annotations.Test;
+
+public class TenstNGAnotation_1 {
+    @Test
+    public void test1(){
+
+        System.out.println("inside test 1");
+    }
+}
