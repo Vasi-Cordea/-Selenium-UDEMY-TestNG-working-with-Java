9934698c0af1a3cda6d057cfdc27c50294ce2465
Vasi-Cordea <150058199+Vasi-Cordea@users.noreply.github.com>
2024-04-02 15:51:36 +0300

    AFTER

--- src/LearningTestNG/TenstNGAnotation_1.java
+++ src/LearningTestNG/TenstNGAnotation_1.java
@@ -1,25 +1,32 @@
 package LearningTestNG;
 
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class TenstNGAnotation_1 {
     @BeforeMethod
-    public void beforeMethod(){
+    public void beforeMethod() {
 
         System.out.println("Opening Browser");
     }
 
 
     @Test
-    public void test1(){
+    public void test1() {
 
         System.out.println("inside test 1");
     }
+
     @Test
-    public void test2(){
+    public void test2() {
         System.out.println("inside test 2");
 
 
+    }
+
+    @AfterMethod
+    public void afterMethod() {
+        System.out.println("Closing Browser");
     }
 }
